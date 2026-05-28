4b5cbbab3b91344c1da8fc2cac694fe4b508c2c3
ungjj1 <kurokhtin_2016@mail.ru>
2026-05-28 03:35:23 +0300

    Страница с периферией

--- PCAdminApp/Pages/PeripheriaPage.xaml.cs
+++ PCAdminApp/Pages/PeripheriaPage.xaml.cs
@@ -26,16 +26,79 @@ namespace PCAdminApp.Pages
         public PeripheriaPage(User currentuser)
         {
             InitializeComponent();
-            LoadData();
             this.currentuser = currentuser;
+            ListPheripheiral.ItemsSource = App.db.PeripheralProfile.ToList();
+            LoadData();
+            LoadSortOptions();
+        }
+
+        private void LoadSortOptions()
+        {
+            SortComboBox.Items.Clear();
+            SortComboBox.Items.Add("Без фильтров");
+            SortComboBox.Items.Add("По названию профиля (А-Я)");
+            SortComboBox.Items.Add("По названию профилья (Я-А)");
+            SortComboBox.SelectedIndex = 0;
         }
 
         private void LoadData()
         {
-            ListPheripheiral.ItemsSource = App.db.PeripheralProfile.ToList();
+            allper = App.db.PeripheralProfile.ToList();
+            RefreshData();
+        }
+        private void RefreshData()
+        {
+            if (allper == null)
+            {
+                return;
+            }
+
+            string search = TBSeacrh?.Text;
+            IEnumerable<PeripheralProfile> filteredper = allper;
+
+            if (search != null)
+            {
+                filteredper = filteredper.Where(p =>
+                (p.ProfileName != null && p.ProfileName.ToLower().Contains(search)) ||
+                (p.MouseModel != null && p.MouseModel.ToLower().Contains(search)) ||
+                (p.KeyboardModel != null && p.KeyboardModel.ToLower().Contains(search)) ||
+                (p.HeadsetModel != null && p.HeadsetModel.ToLower().Contains(search)) ||
+                (p.MonitorModel != null && p.MonitorModel.ToLower().Contains(search))
+                );
+            }
+
+            if (SortComboBox.SelectedItem != null && SortComboBox.SelectedItem.ToString() != "Без фильтров")
+            {
+                string selectedSort = SortComboBox.SelectedItem.ToString();
+
+                switch (selectedSort)
+                {
+                    case "По названию профиля (А-Я)":
+                        filteredper = filteredper.OrderBy(c => c.ProfileName);
+                        break;
+
+                    case "По названию профиля (Я-А)":
+                        filteredper = filteredper.OrderByDescending(c => c.ProfileName);
+                        break;
+                }
+            }
+            ListPheripheiral.ItemsSource = filteredper.ToList();
         }
 
-        private void BtnEditPeripheries_Click(object sender, RoutedEventArgs e)
+        private void TBSeacrh_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            RefreshData();
+        }
+        private void SortComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            RefreshData();
+        }
+        private void BtnAddPeripProfile_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnEditPeripheria_Click(object sender, RoutedEventArgs e)
         {
 
         }
